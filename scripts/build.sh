e0bceecb956c0edf2cbe5ca400b8196277b858dd
Ben Pauley <ben.pauley@paconsulting.com>
2025-09-02 14:07:16 +0100

    Fix further broken image links

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,6 +15,7 @@ docker exec -it $(docker ps --filter="name=landing" -q) bash -c "make html"
 
 cp -r api-documentation/build/html/* docs/api-documentation
 cp -r data-model-user-guide/build/html/* docs/data-model-user-guide
+cp -r data-model-user-guide/source/Data\ Model\ HTML\ Guide docs/data-model-user-guide
 cp -r user-portal-documentation/build/html/* docs/user-portal-documentation
 cp -a portal-access/build/html/* docs
 cp -a docs/api-documentation/shared/*.html docs
