88086036ee236f0b12400a7e51e648649865857d
Ben Pauley <ben.pauley@paconsulting.com>
2025-09-10 16:54:24 +0100

    Update build script to bring up docker containers

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-rm -rf docs
-
-mkdir -p docs/api-documentation
-mkdir -p docs/data-model-user-guide
-mkdir -p docs/user-portal-documentation
-
 docker exec -it $(docker ps --filter="name=api-documentation" -q) bash -c "redocly build-docs specs/openapi.yaml -o source/_static/redoc.html && make html"
 docker exec -it $(docker ps --filter="name=data-model-user-guide" -q) bash -c "make html"
 docker exec -it $(docker ps --filter="name=user-portal-documentation" -q) bash -c "make html"
@@ -19,6 +13,3 @@ cp -r data-model-user-guide/source/Data\ Model\ HTML\ Guide docs/data-model-user
 cp -r user-portal-documentation/build/html/* docs/user-portal-documentation
 cp -a portal-access/build/html/* docs
 cp -a docs/api-documentation/shared/*.html docs
-cp CNAME docs
-cp .nojekyll docs
-cp robots.txt docs
