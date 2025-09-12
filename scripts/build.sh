805a275a0dbb4d999b5422a1451328c6df8a6592
Ben Pauley <ben.pauley@paconsulting.com>
2025-09-12 15:07:28 +0100

    Remove build files from main branch

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,7 @@ docker exec -e PR_NUMBER=$PR_NUMBER $(docker ps --filter="name=data-model-user-g
 docker exec -e PR_NUMBER=$PR_NUMBER $(docker ps --filter="name=user-portal-documentation" -q) bash -c "make html"
 docker exec -e PR_NUMBER=$PR_NUMBER $(docker ps --filter="name=landing" -q) bash -c "make html"
 
+mkdir docs
 cp -r api-documentation/build/html/* docs/api-documentation
 cp -r data-model-user-guide/build/html/* docs/data-model-user-guide
 cp -r data-model-user-guide/source/Data\ Model\ HTML\ Guide docs/data-model-user-guide
