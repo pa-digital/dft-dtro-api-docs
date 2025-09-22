1766b66c1687f2c0436f2e467883ca6cf6c2ac64
Ben Pauley <ben.pauley@paconsulting.com>
2025-09-22 16:27:23 +0100

    Fix URLs for shared pages

--- portal-access/source/conf.py
+++ portal-access/source/conf.py
@@ -34,5 +34,7 @@ else:
 
 html_context = {
     "is_landing_page": True,
-    "base_path": f"/dft-dtro-api-docs-staging/pr-{pr_number}/" if pr_number else "",
+    "base_path": (
+        f"/dft-dtro-api-docs-staging/pr-{pr_number}/" if pr_number else html_baseurl
+    ),
 }
