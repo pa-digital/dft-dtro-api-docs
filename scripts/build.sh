3a3fa644d405842405a1a6be2c05bd55e4f05c84
Ben Pauley <ben.pauley@paconsulting.com>
2026-01-08 11:43:41 +0000

    Further sphinx-multiversion config

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,16 +2,10 @@
 
 set -e
 
-docker exec -e PR_NUMBER=$PR_NUMBER $(docker ps --filter="name=api-documentation" -q) \
-  bash -c "redocly build-docs specs/openapi.yaml -o source/_static/redoc.html && make html"
-
-docker exec -e PR_NUMBER=$PR_NUMBER $(docker ps --filter="name=user-portal-documentation" -q) \
-  bash -c "make html"
-
-docker exec -e PR_NUMBER=$PR_NUMBER $(docker ps --filter="name=landing" -q) \
-  bash -c "make html"
-
-sphinx-multiversion data-model-user-guide/source data-model-user-guide/build
+docker exec -e PR_NUMBER=$PR_NUMBER $(docker ps --filter="name=api-documentation" -q) bash -c "redocly build-docs specs/openapi.yaml -o source/_static/redoc.html && make html"
+docker exec -e PR_NUMBER=$PR_NUMBER $(docker ps --filter="name=data-model-user-guide" -q) bash -c "cd /workspace/data-model-user-guide && make html"
+docker exec -e PR_NUMBER=$PR_NUMBER $(docker ps --filter="name=user-portal-documentation" -q) bash -c "make html"
+docker exec -e PR_NUMBER=$PR_NUMBER $(docker ps --filter="name=landing" -q) bash -c "make html"
 
 mkdir -p docs/api-documentation
 mkdir -p docs/data-model-user-guide
@@ -21,9 +15,7 @@ mkdir -p docs/api-documentation
 cp -r api-documentation/build/html/* docs/api-documentation
 cp -r api-documentation/collections/dtro-integration.postman_collection.json docs/api-documentation/_static/dtro-integration.postman_collection.json
 cp -r api-documentation/collections/dtro-production.postman_collection.json docs/api-documentation/_static/dtro-production.postman_collection.json
-
-cp -r data-model-user-guide/build/* docs/data-model-user-guide  
-
+cp -r data-model-user-guide/build/html/* docs/data-model-user-guide
 cp -r data-model-user-guide/source/Data\ Model\ HTML\ Guide docs/data-model-user-guide
 cp -r user-portal-documentation/build/html/* docs/user-portal-documentation
 cp -a portal-access/build/html/* docs
