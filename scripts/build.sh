7cda3ae8496a67d3288a799e7c167be413683d34
Ben Pauley <ben.pauley@paconsulting.com>
2026-01-08 11:35:34 +0000

    Update build script to work with sphinx-multiversion

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,10 +2,16 @@
 
 set -e
 
-docker exec -e PR_NUMBER=$PR_NUMBER $(docker ps --filter="name=api-documentation" -q) bash -c "redocly build-docs specs/openapi.yaml -o source/_static/redoc.html && make html"
-docker exec -e PR_NUMBER=$PR_NUMBER $(docker ps --filter="name=data-model-user-guide" -q) bash -c "make html"
-docker exec -e PR_NUMBER=$PR_NUMBER $(docker ps --filter="name=user-portal-documentation" -q) bash -c "make html"
-docker exec -e PR_NUMBER=$PR_NUMBER $(docker ps --filter="name=landing" -q) bash -c "make html"
+docker exec -e PR_NUMBER=$PR_NUMBER $(docker ps --filter="name=api-documentation" -q) \
+  bash -c "redocly build-docs specs/openapi.yaml -o source/_static/redoc.html && make html"
+
+docker exec -e PR_NUMBER=$PR_NUMBER $(docker ps --filter="name=user-portal-documentation" -q) \
+  bash -c "make html"
+
+docker exec -e PR_NUMBER=$PR_NUMBER $(docker ps --filter="name=landing" -q) \
+  bash -c "make html"
+
+sphinx-multiversion data-model-user-guide/source data-model-user-guide/build
 
 mkdir -p docs/api-documentation
 mkdir -p docs/data-model-user-guide
@@ -15,7 +21,9 @@ mkdir -p docs/api-documentation
 cp -r api-documentation/build/html/* docs/api-documentation
 cp -r api-documentation/collections/dtro-integration.postman_collection.json docs/api-documentation/_static/dtro-integration.postman_collection.json
 cp -r api-documentation/collections/dtro-production.postman_collection.json docs/api-documentation/_static/dtro-production.postman_collection.json
-cp -r data-model-user-guide/build/html/* docs/data-model-user-guide
+
+cp -r data-model-user-guide/build/* docs/data-model-user-guide  
+
 cp -r data-model-user-guide/source/Data\ Model\ HTML\ Guide docs/data-model-user-guide
 cp -r user-portal-documentation/build/html/* docs/user-portal-documentation
 cp -a portal-access/build/html/* docs
